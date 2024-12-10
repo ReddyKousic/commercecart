57bf03ed4f4adc77c787f1c38040ae501aefba45
ReddyKousic <85285392+ReddyKousic@users.noreply.github.com>
2024-12-10 14:26:27 +0530

    Added a way to block big screen requests

---
+++ src/routes/not-supported/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	let { data }: { data: PageData } = $props();
+	import MainLogo from '$lib/assets/MainLogo.png';
+</script>
+
+<div class="not-supported">
+	<img src={MainLogo} alt="" class="w-1/4" />
+	<h1>This application is only available for small screens.</h1>
+	<a href="https://koastec.com" class="text-sm text-black">
+		<p class="underline">Koastec - High Performance Systems</p>
+	</a>
+</div>
+
+<style>
+	.not-supported {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-evenly;
+		align-items: center;
+		height: 100vh;
+		text-align: center;
+	}
+</style>
